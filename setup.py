f439ddd9de35f2722493ac805e1956d455f363a4
abrougui <bahaeddine.abrougui@lidl.com>
2023-10-06 12:50:04 +0200

    Use setuptools_scm for gitHub workflow versioning

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ import os
 
 from setuptools import setup
 
-setup(
-    version=os.environ['ARTIFACT_LABEL']
-)
+# If build workflow is triggered from ADO, ARTIFACT_LABEL exists
+try:
+    setup(
+        version=os.environ['ARTIFACT_LABEL']
+    )
+# If build is triggered for gitHub, use setuptools_scm to fetch tag as version
+except KeyError:
+    pass
