dc212487da4f12f2a61134f61c8f64689cb18c68
abrougui <bahaeddine.abrougui@lidl.com>
2023-10-06 13:56:10 +0200

    Update setting_databricks_connect.rst doc

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ config = toml.load('../pyproject.toml')
 project = config['project']['name']
 copyright = f'2023-{date.today().year} lidl e-commerce'
 author = ', '.join(author['name'] for author in config['project']['authors'])
-version = os.environ.get('ARTIFACT_LABEL', 'pre-release')
+version = os.environ.get('ARTIFACT_LABEL', audeer.git_repo_version())
 title = 'Documentation'
 
 
