a35df601e92453c9a978ccd023a8739ad4844009
abrougui <bahaeddine.abrougui@lidl.com>
2023-10-09 15:43:20 +0200

    Upgrade from deprecated github output approach

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ import os
 
 from setuptools import setup
 
-# If build workflow is triggered from ADO, ARTIFACT_LABEL exists
+# If build workflow is triggered from ADO or it's a pre-release,
+# ARTIFACT_LABEL exists
 try:
     setup(
         version=os.environ['ARTIFACT_LABEL']
     )
-# If build is triggered for gitHub, use setuptools_scm to fetch tag as version
+# If build is triggered for gitHub and it's not a pre-release,
+# use setuptools_scm to fetch tag as version
 except KeyError:
     setup(
         use_scm_version=True
