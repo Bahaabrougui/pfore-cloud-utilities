c92c3aea880c4f879a68dcac854e8d39206f6b52
abrougui <bahaeddine.abrougui@lidl.com>
2023-10-10 12:32:35 +0200

    Update version fetching in docs and adjust comment in publish pipeline

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ config = toml.load('../pyproject.toml')
 project = config['project']['name']
 copyright = f'2023-{date.today().year} lidl e-commerce'
 author = ', '.join(author['name'] for author in config['project']['authors'])
-version = os.environ.get('ARTIFACT_LABEL', audeer.git_repo_version())
+version = os.environ.get('ARTIFACT_LABEL', audeer.git_repo_tags(v=True)[-1])
 title = 'Documentation'
 
 
