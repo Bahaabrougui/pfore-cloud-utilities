6732805bc128d2a0a0c9e65a48ff29dae1a3c133
abrougui <bahaeddine.abrougui@lidl.com>
2023-10-10 12:50:18 +0200

    Remove artifact_label from version statement

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@ config = toml.load('../pyproject.toml')
 project = config['project']['name']
 copyright = f'2023-{date.today().year} lidl e-commerce'
 author = ', '.join(author['name'] for author in config['project']['authors'])
-version = os.environ.get('ARTIFACT_LABEL', audeer.git_repo_tags(v=True)[-1])
+version = audeer.git_repo_tags(v=True)[-1]
+raise ValueError(version)
 title = 'Documentation'
 
 
