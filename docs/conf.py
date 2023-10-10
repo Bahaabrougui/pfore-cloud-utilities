1325125637ea878055664dc8d4122ef0a9b482c1
abrougui <bahaeddine.abrougui@lidl.com>
2023-10-10 14:10:28 +0200

    Debug

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,12 @@ project = config['project']['name']
 copyright = f'2023-{date.today().year} lidl e-commerce'
 author = ', '.join(author['name'] for author in config['project']['authors'])
 version = os.environ.get('ARTIFACT_LABEL', audeer.git_repo_version())
-raise ValueError('Ah', version, os.environ['ARTIFACT_LABEL'])
+print(version)
+print(os.environ['ARTIFACT_LABEL'])
+print(audeer.git_repo_tags()[-1])
+import git
+print(git.Git().tags()[-1])
+raise ValueError()
 title = 'Documentation'
 
 
