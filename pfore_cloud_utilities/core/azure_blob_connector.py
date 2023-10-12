90706069b1c6ddb610adf94b4d911accb363c704
BahaEddine Abrougui <38329838+Bahaabrougui@users.noreply.github.com>
2023-10-12 12:43:15 +0200

    Update Azure authentication method
    * Remove tenant_id from required args when using MI
    
    * Use different check approach for auth method
    
    * Update docstrings
    
    ---------
    
    Co-authored-by: abrougui <bahaeddine.abrougui@lidl.com>

--- pfore_cloud_utilities/core/azure_blob_connector.py
+++ pfore_cloud_utilities/core/azure_blob_connector.py
@@ -12,24 +12,28 @@ from .singleton import Singleton
 class AzureBlobConnector(metaclass=Singleton):
     """Singleton class to interact with an Azure Blob Storage container.
 
-    The classes handles connection, reads and writes inside the container.
+    The classes handle connection, reads and writes inside the container.
 
-    Connection is created using either a managed identity or an SPN.
+    Connection is created using either a Managed Identity or an SPN.
 
-    Upon instantiation, if spn_client_id or spn_client_secret are set to None,
-    the client will assume Managed Identity as an authentication method.
+    Upon instantiation, if azure_tenant_id, spn_client_id and spn_client_secret
+    are all set to None, the client will assume Managed Identity
+    as an authentication method.
 
     Args:
         account_url: Storage Account url, required
-        azure_tenant_id: Azure tenant ID, required
+        azure_tenant_id: Azure tenant ID
         spn_client_id: Service Principal client ID
         spn_client_secret: Service Principal client secret
+
+    Raises:
+        ValueError: If one of the arguments for SPN-based auth is missing
 
     """
     def __init__(
             self,
             account_url: str,
-            azure_tenant_id: str,
+            azure_tenant_id: str = None,
             spn_client_id: str = None,
             spn_client_secret: str = None,
             ) -> None:
@@ -37,9 +41,18 @@ class AzureBlobConnector(metaclass=Singleton):
         # account_url is saved as class attribute to be used for comparison
         # during class instance creation
         self._account_url = account_url
-        if spn_client_id is None or spn_client_secret is None:
+
+        if (spn_client_id is None and spn_client_secret is None
+                and azure_tenant_id is None):
             credentials = DefaultAzureCredential()
         else:
+            if None in (azure_tenant_id, spn_client_secret, spn_client_secret):
+                raise ValueError(
+                    'One of the required arguments for SPN-based '
+                    'authentication is not specified. Please either '
+                    'specify all required arguments for SPN-based auth, '
+                    'or don\'t specify any argument at all for MI-based auth.'
+                )
             credentials = ClientSecretCredential(
                 tenant_id=azure_tenant_id,
                 client_id=spn_client_id,
